8e40483925d9307af65104446a3da855ccd0da6d
lgy1027 <lgy10271416@gmail.com>
2026-06-06 12:20:13 +0800

    feat(api): /v1/history 列表/筛选/分页/删除

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,6 +5,7 @@ from app.api.upload import router as upload_router
 from app.api.speakers import router as speakers_router
 from app.api.health import router as health_router
 from app.api.exports import router as exports_router
+from app.api.history import router as history_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
@@ -12,3 +13,4 @@ api_router.include_router(ws_router)
 api_router.include_router(upload_router)
 api_router.include_router(speakers_router)
 api_router.include_router(exports_router)
+api_router.include_router(history_router)
